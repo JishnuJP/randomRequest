f15a77c11352a03c37e2ae5d142cb8470d1e3f89
JishnuJP <jishnujprksh007@gmail.com>
2021-06-13 21:34:59 +0530

    Splitted the randomRequest into three class - randomProxy, randomAgent and randomRequest(randomProxy,randomAgent). randomProxy class is the python wrapper for proxy-list api, country_code.json has the data of ISO code of different countries, help in picking proxy from a specific country

--- randomRequest.py
+++ randomRequest.py
@@ -1,56 +1,107 @@
+import requests
+import json
 from random import choice
 
-proxy_type = ['elite','anonymous','transparent']
-with open('useragents.txt') as f:
-    agents = [line.rstrip() for line in f]
 
-class randomRequest():
+
+class randomAgent:
+	def __init__(self,**kwargs):
+		with open('useragents.txt') as f:
+		    self.agents = [line.rstrip() for line in f]		
+		for key, value in kwargs.items():
+			if key == 'Browser':
+				[self.agents.remove(agent) for agent in self.agents if value not in agent]
+			## possibility of an exception when the value of 'Browser' is not matching
+	def getRandomAgent(self):
+		return choice(self.agents)
+
+
+
+
+
+class randomProxy:
+	def __init__(self,**kwargs):
+		self.url = 'https://www.proxy-list.download/api/v1/get'
+		self.types = ['http','https','socks4','socks5']
+		self.anons = ['transparent','anonymous','elite']
+		for key, value in kwargs.items():
+			if key == 'Type':
+				if value in self.types:
+					self.url = self.url + f'?type={value}'
+				else:
+					raise TypeError(f'''{Type} is Invalid protocol, 
+						Valid protocols are {self.types}''')		
+			elif key =='Anon':		
+				if value in self.anons:
+					self.url = self.url+ f'&anon={Anon}'
+				else:
+					pass
+			elif key == 'Country':
+				validity,self.code = self.getCodes(value)
+				if validity:
+					self.url = self.url +f'&country={self.code}'
+				else:
+					pass
+
+		response = requests.get(self.url)
+		if response.status_code != 200:
+			raise Exception(f'Request Failed url is : {self.url}')
+		self.proxies = response.content.decode('utf-8')
+		self.proxies = self.proxies.split('\r\n')
+		self.proxies.remove('')
+
+	def getCodes(self, Country):
+		with open('country_codes.json') as f:
+			country_codes = json.load(f)
+		if Country == None:
+			return False, None
+		for i in country_codes:
+			if Country.upper() in i.upper():
+				return True,country_codes[i]
+	def getRandomProxy(self):
+		return choice(self.proxies)
+
+
+
+
+class randomRequest(randomAgent, randomProxy):
 	def __init__(self,**kwargs):
 		# expected arguments = > header, proxy, User-Agent 
+		#super().__init__(**kwargs)
+		randomAgent.__init__(self,**kwargs)
+		randomProxy.__init__(self,**kwargs)
 		self.head = {}
 		for key, value in kwargs.items():
 			if key =='header' :
 				self.head = value
 			elif key == 'proxy':
 				if type(value)==list:
-					self.proxy = value
+					self.proxies = value
 				elif type(value) == str:
-					if value in proxy_type:
-						self.proxy = self.getProxy(value)
-					else:
-						raise Exception('''{} is not a valid proxy_type, 
-							Try ({})'''.format(value,proxy_type))
+					self.proxies = [value]
 				else:
-					raise TypeError('proxy should be list or str, not ',type(value))
-
-			elif key == 'UserAgent':
-				if value == 'random':
-					self.head['User-Agent'] = self.getUserAgent()
-				else:
-					self.head['User-Agent'] = value
-
-			else:
-				raise Exception('''Invalid parameter - {},
-				 accepted parameters are proxy, UserAgent ,header,{},'''.format(value,proxy_type))
-
-	def getProxy(self,value):
-		if value not in proxy_type:
-			raise Exception('''{} is not a valid proxy_type, 
-							Try ({})'''.format(value,proxy_type))
-			return
-		# random selection of proxy
-		return 'random_proxy'
-	def getUserAgent(self):
-		#include some paramets that can filter the random selection of useragent
-		return choice(agents)
-
+					raise TypeError(f'{type(value)} is not acceptable, give list or string') 
+			
+				
+	def verifty(self, headers, proxies):
+		try:
+			resp  = requests.get('https://httpbin.org/get',headers = headers, proxies = proxies)
+		except:
+			return False
+		if resp.status_code == 200:
+			return True
+		else:
+			 return False
 
 
 
 
 if __name__ == '__main__':
-	'''url = 'https://httpbin.org/ip'
-				response = requeste.get(url,randomRequest())
-				print(response.json())'''
-	print(randomRequest(UserAgent = 'random').head)
-	print(randomRequest(proxy = 'elite').proxy)
+	randReq = randomRequest(Type = 'https')
+	agent = randReq.getRandomAgent()
+	proxy = randReq.getRandomProxy()
+	print(f'Agent : \n {agent} \n Proxy: \n {proxy}')
+	if randReq.verifty(headers = {'User-Agent':agent},proxies = {'https':proxy}):
+		print('Request is Valid')
+	else:
+		print('Request is Invalid')
